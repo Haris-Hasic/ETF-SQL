5b96e461aa87beba7f101beb6e7f870ff6325a7f
Haris-Hasic <hhasic2@etf.unsa.ba>
2014-11-01 23:52:10 +0100

    Napravljen početni prozor.

--- etf-SQL/src/Prozori/GlavniProzor.java
+++ etf-SQL/src/Prozori/GlavniProzor.java
@@ -3,10 +3,19 @@ package Prozori;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Font;
 
 public class GlavniProzor {
 
-	private JFrame frame;
+	private JFrame frmEtfSql;
+	private final JTextPane textPane = new JTextPane();
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class GlavniProzor {
 			public void run() {
 				try {
 					GlavniProzor window = new GlavniProzor();
-					window.frame.setVisible(true);
+					window.frmEtfSql.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,68 @@ public class GlavniProzor {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmEtfSql = new JFrame();
+		frmEtfSql.setTitle("ETF SQL");
+		frmEtfSql.setIconImage(Toolkit.getDefaultToolkit().getImage(GlavniProzor.class.getResource("/Resursi/ETF_Logo.jpg")));
+		frmEtfSql.setBounds(100, 100, 450, 300);
+		frmEtfSql.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frmEtfSql.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frmEtfSql.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		textPane.setBounds(10, 88, 414, 126);
+		panel.add(textPane);
+		
+		JButton btnNewButton = new JButton("Commit");
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnNewButton.setBounds(345, 228, 79, 23);
+		panel.add(btnNewButton);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.setBounds(0, 0, 434, 21);
+		panel.add(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		mnFile.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnFile);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		mnEdit.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnEdit);
+		
+		JMenu mnView = new JMenu("View");
+		mnView.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnView);
+		
+		JMenu mnSearch = new JMenu("Search");
+		mnSearch.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnSearch);
+		
+		JMenu mnText = new JMenu("Text");
+		mnText.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnText);
+		
+		JMenu mnSession = new JMenu("Session");
+		mnSession.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnSession);
+		
+		JMenu mnScript = new JMenu("Script");
+		mnScript.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnScript);
+		
+		JMenu mnTools = new JMenu("Tools");
+		mnTools.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnTools);
+		
+		JMenu mnWindow = new JMenu("Window");
+		mnWindow.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnWindow);
+		
+		JMenu mnHelp = new JMenu("Help");
+		mnHelp.setFont(new Font("Arial", Font.PLAIN, 12));
+		menuBar.add(mnHelp);
 	}
-
 }
