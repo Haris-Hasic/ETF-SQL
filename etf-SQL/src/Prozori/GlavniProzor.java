86c0c9e4b5fb8535e860144e68ca9e0671186fcc
Haris-Hasic <hhasic2@etf.unsa.ba>
2014-11-02 20:16:37 +0100

    lol

--- etf-SQL/src/Prozori/GlavniProzor.java
+++ etf-SQL/src/Prozori/GlavniProzor.java
@@ -3,19 +3,32 @@ package Prozori;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+
 import java.awt.Toolkit;
 import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+
 import java.awt.Font;
+
+import javax.swing.JTextField;
+
+import java.awt.SystemColor;
+
+import javax.swing.JMenuItem;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GlavniProzor {
 
 	private JFrame frmEtfSql;
 	private final JTextPane textPane = new JTextPane();
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -45,9 +58,10 @@ public class GlavniProzor {
 	 */
 	private void initialize() {
 		frmEtfSql = new JFrame();
+		frmEtfSql.setResizable(false);
 		frmEtfSql.setTitle("ETF SQL");
 		frmEtfSql.setIconImage(Toolkit.getDefaultToolkit().getImage(GlavniProzor.class.getResource("/Resursi/ETF_Logo.jpg")));
-		frmEtfSql.setBounds(100, 100, 450, 300);
+		frmEtfSql.setBounds(100, 100, 703, 500);
 		frmEtfSql.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frmEtfSql.getContentPane().setLayout(new BorderLayout(0, 0));
@@ -55,17 +69,17 @@ public class GlavniProzor {
 		JPanel panel = new JPanel();
 		frmEtfSql.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		textPane.setBounds(10, 88, 414, 126);
+		textPane.setBounds(14, 161, 660, 255);
 		panel.add(textPane);
 		
-		JButton btnNewButton = new JButton("Commit");
+		JButton btnNewButton = new JButton("Execute");
 		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 11));
-		btnNewButton.setBounds(345, 228, 79, 23);
+		btnNewButton.setBounds(595, 428, 79, 23);
 		panel.add(btnNewButton);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setFont(new Font("Arial", Font.PLAIN, 12));
-		menuBar.setBounds(0, 0, 434, 21);
+		menuBar.setBounds(0, 0, 684, 21);
 		panel.add(menuBar);
 		
 		JMenu mnFile = new JMenu("File");
@@ -92,6 +106,16 @@ public class GlavniProzor {
 		mnSession.setFont(new Font("Arial", Font.PLAIN, 12));
 		menuBar.add(mnSession);
 		
+		JMenuItem mntmConnect = new JMenuItem("Connect...");
+		mntmConnect.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+
+			}
+		});
+		mntmConnect.setFont(new Font("Arial", Font.PLAIN, 12));
+		mnSession.add(mntmConnect);
+		
 		JMenu mnScript = new JMenu("Script");
 		mnScript.setFont(new Font("Arial", Font.PLAIN, 12));
 		menuBar.add(mnScript);
@@ -107,5 +131,26 @@ public class GlavniProzor {
 		JMenu mnHelp = new JMenu("Help");
 		mnHelp.setFont(new Font("Arial", Font.PLAIN, 12));
 		menuBar.add(mnHelp);
+		
+		textField = new JTextField();
+		textField.setBackground(SystemColor.menu);
+		textField.setBounds(14, 429, 571, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnRollback = new JButton("Rollback");
+		btnRollback.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnRollback.setBounds(559, 86, 115, 63);
+		panel.add(btnRollback);
+		
+		JButton btnCreateSavepoint = new JButton("Create Savepoint");
+		btnCreateSavepoint.setFont(new Font("Arial", Font.PLAIN, 11));
+		btnCreateSavepoint.setBounds(434, 86, 115, 63);
+		panel.add(btnCreateSavepoint);
+		
+		JButton button_2 = new JButton("Commit");
+		button_2.setFont(new Font("Arial", Font.PLAIN, 11));
+		button_2.setBounds(309, 86, 115, 63);
+		panel.add(button_2);
 	}
 }
